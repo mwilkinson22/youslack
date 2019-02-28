05e7065e87e170574babf8f31b6026102a488a59
Mark Wilkinson <markwilko22@gmail.com>
2019-02-28 14:57:04 +0000

    Imported axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const _ = require("lodash");
+const axios = require("axios");
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
