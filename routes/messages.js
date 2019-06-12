267300590d4f09c7e4116b2c6bfa031103adddc3
Mark Wilkinson <markwilko22@gmail.com>
2019-06-12 09:48:55 +0100

    Made the entire process opt-in, even in channels

--- routes/messages.js
+++ routes/messages.js
@@ -1,15 +1,11 @@
 //Modules
 const _ = require("lodash");
 const axios = require("axios");
-const { slackAuth, youtrackAuth } = require("../config/keys");
+const { youtrackAuth } = require("../config/keys");
 const mongoose = require("mongoose");
 const Token = mongoose.model("tokens");
 
 //Set axios headers
-const slackHeaders = {
-	"Content-type": "application/json",
-	Authorization: slackAuth
-};
 const youtrackHeaders = {
 	"Content-type": "application/json",
 	Authorization: youtrackAuth
@@ -24,14 +20,20 @@ function escapeChars(text) {
 }
 
 module.exports = app => {
-	app.post("/", (req, res) => {
+	app.post("/", async (req, res) => {
 		const { event, challenge } = req.body;
 
 		if (event) {
 			const { text, channel, ts, subtype, user } = event;
+			const token = await Token.findOne({ user_id: user });
 
 			//IMPORTANT - Need this if to prevent infinite loops
-			if (subtype !== "bot_message" && text) {
+			if (subtype !== "bot_message" && token && text) {
+				const headers = {
+					"Content-type": "application/json",
+					Authorization: `Bearer ${token.access_token}`
+				};
+
 				const matches = _.uniq(
 					text.match(
 						/(WEB|IM|WSUP|WTST|EN|LENS|ICN|WIKI|nService|STAN|TOP|ILL|MTP|MISC)-\d+/gi
@@ -49,7 +51,7 @@ module.exports = app => {
 								matches.length
 							} issues in one message?! Do you want Skynet?! Because this is how you get Skynet!`
 						},
-						{ headers: slackHeaders }
+						{ headers }
 					);
 				} else {
 					_.map(matches, async issue => {
@@ -73,22 +75,9 @@ module.exports = app => {
 								thread_ts: ts,
 								text: text.length < 3000 ? text : `${text.substr(0, 2997)}...`
 							};
-							const message = await axios.post(url, params, {
-								headers: slackHeaders
+							await axios.post(url, params, {
+								headers
 							});
-
-							if (!message.data.ok) {
-								//DM. Try again if user is authenticated
-								const token = await Token.findOne({ user_id: user });
-								if (token) {
-									await axios.post(url, params, {
-										headers: {
-											"Content-type": "application/json",
-											Authorization: `Bearer ${token.access_token}`
-										}
-									});
-								}
-							}
 						}
 					});
 				}
